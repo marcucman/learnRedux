4085005c269b06ddd3e6e68f1d5df2ec6df40366
Marc Ucman <marcucman@yahoo.com>
2017-03-15 19:14:04 -0500

    Add support for CHANGE_NAME action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -5,16 +5,41 @@ console.log('Starting redux example');
 // you typically use one Store to store your entire application
 // REDUCER = pure function that takes your existing and state and action as arguments and compute a new state
 // ES5 way of assigning default values
-var reducer = (state, action) => {
-  state = state || {name: 'Anonymous'}; // create default
-};
+// var reducer = (state, action) => {
+//   state = state || {name: 'Anonymous'}; // create default
+//
+//   console.log('New action', action);
+// };
 
 // ES6 way of assigning default values
 var reducer = (state = {name: 'Anonymous'}, action) => {
-  return state;
+  console.log('New action', action);
+
+  // HANDLE ACTION
+  switch(action.type) {
+    case 'CHANGE_NAME': // generate new state with name in action used to replace old name
+      return {
+        ...state,
+        name: action.name
+      };
+    default:
+      return state;
+  }
 };
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState(); // return state object
 console.log('currentState', currentState);
+
+// ACTION = object, must have 'type' property
+var action = {
+  type: 'CHANGE_NAME', // action name
+  name: 'Brian' // value to pass
+};
+
+// DISPATCH ACTION
+store.dispatch(action);
+
+// check that action changed the name
+console.log('Name should be Brian', store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -10,6 +10,17 @@ var stateDefault = {
 };
 
 var reducer = (state = stateDefault, action) => {
+  console.log('New action', action);
+  // HANDLE ACTION
+  switch(action.type) {
+    case 'CHANGE_SEARCHTEXT': // generate new state with name in action used to replace old name
+      return {
+        ...state,
+        searchText: action.searchText
+      };
+    default:
+      return state;
+  }
   return state;
 };
 
@@ -19,3 +30,15 @@ var store = redux.createStore(reducer);
 // 3 GET STATE, return state object
 var currentState = store.getState(); // return state object
 console.log('currentState', currentState);
+
+// ACTION = object, must have 'type' property
+var action = {
+  type: 'CHANGE_SEARCHTEXT', // action name
+  searchText: 'new text' // value to pass
+};
+
+// DISPATCH ACTION
+store.dispatch(action);
+
+// check that action changed the name
+console.log('searchText should be different', store.getState());
